13525b7bfa4e0570405e192e5889bc33bb7b0382
Jar1hon <te3s.meqa@gmail.com>
2023-11-11 14:51:19 +0500

    Fixed

--- 8 kyu/invert-values.js
+++ 8 kyu/invert-values.js
@@ -2,3 +2,18 @@
 // Given a set of numbers, return the additive inverse of each. Each positive becomes negatives, and the negatives become positives.
 
 //✅ SOLUTIONS:
+function invert(array) {
+  let total = [];
+  for (let i = 0; i < array.length; i++) {
+    if (array[i] > 0) {
+      total.push(0 - array[i]);
+    } else if (array[i] < 0) {
+      total.push(0 - array[i]);
+    } else if (array[i] === 0) {
+      total.push(0);
+    }
+  }
+  return total;
+}
+
+console.log(invert([1, -2, 3, -4, 5]));
