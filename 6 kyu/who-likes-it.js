023edc4a9e17865a9c7c44f03de219d4b472e94c
Jar1hon <te3s.meqa@gmail.com>
2023-11-13 21:53:33 +0500

    fixed

--- 6 kyu/who-likes-it.js
+++ 6 kyu/who-likes-it.js
@@ -3,7 +3,6 @@
 
 // Implement the function which takes an array containing the names of people that like an item. It must return the display text as shown in the examples:
 
-"use strict";
 // []                                -->  "no one likes this"
 // ["Peter"]                         -->  "Peter likes this"
 // ["Jacob", "Alex"]                 -->  "Jacob and Alex like this"
